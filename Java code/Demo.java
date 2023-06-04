07cf58fba921ac644e0988725caedd837166b93f
Veningston K <veningstonk@gmail.com>
2023-06-05 02:11:10 +0530

    A new file Sub.java is created and a new line is added to the existing Demo.java

--- Java code/Demo.java
+++ Java code/Demo.java
@@ -2,5 +2,6 @@ class Demo {
 	public static void main(String[] arg) {
 		System.out.println("Hello World!");
 		System.out.println("Hello NIT Srinagar");
+		System.out.println("Hello Veningston here...");	//New Line Added
 	}
 }
---
+++ Java code/Sub.java
@@ -0,0 +1,6 @@
+class Sum{
+	public static void main(String[] arg) {
+		int a = 10, b = 20;
+		System.out.println(a-b);
+	}
+}
