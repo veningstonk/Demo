8c8468410ba4004354b7ac9a9dbcc403043bc255
Veningston K <veningstonk@gmail.com>
2023-06-05 02:29:57 +0530

    A line is removed and a new line is added to Demo.java

--- Java code/Demo.java
+++ Java code/Demo.java
@@ -2,6 +2,6 @@ class Demo {
 	public static void main(String[] arg) {
 		System.out.println("Hello World!");
 		System.out.println("Hello NIT Srinagar");
-		System.out.println("Hello Veningston here...");	//New Line Added
+		System.out.println("I am from CSE Department");	//Another New Line Added
 	}
 }
